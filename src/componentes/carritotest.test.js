64cdb479e0cd4a6855940e3d78b00e3a0b758d98
FernandoMarOrt <fernandomartinezortiz003@gmail.com>
2024-12-16 19:03:03 +0100

    Test unitarios segunda prueba problema con axios

--- src/componentes/carritotest.test.js
+++ src/componentes/carritotest.test.js
@@ -1,6 +1,22 @@
 import { render, screen } from '@testing-library/react';
-import { act } from 'react'; 
+import { act } from 'react';
 import Carrito from './carrito';
+
+// Desactivar las advertencias de deprecación de `act` y el error de importación
+beforeAll(() => {
+  jest.spyOn(global.console, 'error').mockImplementation((message) => {
+    if (
+      !message.includes('Warning: `ReactDOMTestUtils.act` is deprecated') &&
+      !message.includes('Cannot use import statement outside a module')
+    ) {
+      console.error(message);  // Solo muestra otros errores
+    }
+  });
+});
+
+afterAll(() => {
+  console.error.mockRestore(); // Restaurar después de que terminen las pruebas
+});
 
 describe('Carrito', () => {
 
